33f959538df0ecb50c09c5816e4d88b8bcd686ea
Julian Fahrer <julian@fahrer.net>
2019-07-21 08:43:40 -0700

    Set completed to false by default

--- db/migrate/20190721025643_create_todos.rb
+++ db/migrate/20190721025643_create_todos.rb
@@ -2,7 +2,7 @@ class CreateTodos < ActiveRecord::Migration[5.2]
   def change
     create_table :todos do |t|
       t.string :title
-      t.boolean :completed
+      t.boolean :completed, default: false
 
       t.timestamps
     end
