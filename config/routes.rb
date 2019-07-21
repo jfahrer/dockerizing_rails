8a29cb73ebaa8fe86d3a0049c0fc244a483e55de
Julian Fahrer <julian@fahrer.net>
2019-07-21 16:39:25 -0700

    Add a default route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :todos, except: [:new, :edit, :show]
   resources :archived_todos, only: [:create]
   resources :activities, only: [:index]
+
+  root to: 'todos#index'
 end
