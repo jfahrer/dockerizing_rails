2d101624198bdc8579c48fc9bc80f99549e38885
Julian Fahrer <julian@fahrer.net>
2019-07-21 09:10:43 -0700

    Change controller specs to request specs

--- spec/requests/todos_spec.rb
+++ spec/requests/todos_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "Todos", type: :request do
+  let(:valid_attributes) {
+    { title: 'laundry'}
+  }
+
+  let(:invalid_attributes) {
+    { title: '' }
+  }
+
   describe 'GET /todos' do
     it 'lists all todos' do
       Todo.create!(title: 'laundry')
@@ -48,6 +56,59 @@ RSpec.describe "Todos", type: :request do
           expect(response.body).to include("shopping")
         end
       end
+    end
+  end
+
+  describe "POST /todos" do
+    context "with valid params" do
+      it "creates a new Todo" do
+        expect {
+          post todos_path, params: {todo: valid_attributes}
+        }.to change(Todo, :count).by(1)
+        expect(response).to redirect_to(todos_path)
+      end
+    end
+
+    context "with invalid params" do
+      it "redirects to the todo list" do
+        post todos_path, params: {todo: invalid_attributes}
+        expect(response).to redirect_to(todos_path)
+      end
+    end
+  end
+
+  describe "PATCH /todos/:id" do
+    context "with valid params" do
+      let(:new_attributes) {
+        { completed: true }
+      }
+
+      it "updates the requested todo" do
+        todo = Todo.create! valid_attributes
+        expect(todo.completed).to be_falsy
+        patch todo_path(todo), params: {id: todo.to_param, todo: new_attributes}
+        expect(response).to redirect_to(todos_path)
+        todo.reload
+        expect(todo.completed).to be_truthy
+      end
+    end
+
+    context "with invalid params" do
+      it "redirects to the todos" do
+        todo = Todo.create! valid_attributes
+        put todo_path(todo), params: {id: todo.to_param, todo: invalid_attributes}
+        expect(response).to redirect_to(todos_path)
+      end
+    end
+  end
+
+  describe "DELETE /todos/:id" do
+    it "destroys the requested todo" do
+      todo = Todo.create! valid_attributes
+      expect {
+        delete todo_path(todo), params: {id: todo.to_param}
+      }.to change(Todo, :count).by(-1)
+      expect(response).to redirect_to(todos_path)
     end
   end
 end
