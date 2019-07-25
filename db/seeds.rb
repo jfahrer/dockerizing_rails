68ebc539ea294dddf6f093cfb44f6edd39776048
Julian Fahrer <julian@fahrer.net>
2019-07-25 08:27:41 -0700

    Add some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Todo.create!(title: "Laundry", created_at: 51.hours.ago)
+Todo.create!(title: "Bring the trash out", completed: true, created_at: 51.3.hours.ago)
+Todo.create!(title: "Shopping", created_at: 1.hours.ago)
+
+Activity.create!([
+  {name: "todo_created", data: {title: "Laundry"}, created_at: 51.hours.ago},
+  {name: "todo_created", data: {title: "Bring the trash out"}, created_at: 51.3.hours.ago},
+  {name: "todo_marked_as_complete", data: {title: "Bring the trash out"}, created_at: 48.1.hours.ago},
+  {name: "todo_created", data: {title: "Pick the kids up from soccer"}, created_at: 25.3.hours.ago},
+  {name: "todo_deleted", data: {title: "Pick the kids up from soccer"}, created_at: 24.0.hours.ago},
+  {name: "todo_created", data: {title: "Shopping"}, created_at: 1.hours.ago},
+])
+
+0.upto(3) do |i|
+  date = Date.today - i.days
+  ScoreCalculator.call(date)
+end
